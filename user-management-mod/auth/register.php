fbaf614b177966b029054bf8263b5d16a86f27cb
czrnaa <caabendanio@gamil.com>
2026-01-22 00:53:21 +0800

    Draft code of user registration

--- user-management-mod/auth/register.php
+++ user-management-mod/auth/register.php
@@ -1,12 +1,20 @@
 <?php
+require "../utils/user_functions.php";
 
-// global var
-$x = 5;
+$name = $_POST['name'];
+$email = $_POST['email'];
+$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
-function myFunc()
-{
-  // echo "Variable inside x function is: $x";
-}
+$users = loadUsers();
 
-echo "Variable outside x function works is: $x";
-?>
+$users[] = [
+  "id" => count($users) + 1,
+  "name" => $name,
+  "email" => $email,
+  "password" => $password,
+  "role" => "Job Getter",
+  "status" => "active"
+];
+
+saveUsers($users);
+echo "Registration successful";
