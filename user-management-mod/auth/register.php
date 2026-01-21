e7101b68867bea4a66871ae9019a9bbf049da023
czrnaa <caabendanio@gamil.com>
2026-01-22 00:30:35 +0800

    Test built-in live server

--- user-management-mod/auth/register.php
+++ user-management-mod/auth/register.php
@@ -1,5 +1,12 @@
 <?php
 
-echo 'Welcome home';
+// global var
+$x = 5;
 
+function myFunc()
+{
+  // echo "Variable inside x function is: $x";
+}
+
+echo "Variable outside x function works is: $x";
 ?>
